0fe9e1c4a78da9391409460a316e66f7d1f7cb2b
Miguel Pérez <miguel.perez@sembo.se>
2022-05-14 22:39:19 +0200

    update actions

--- Domain/Exceptions/AlreadyExistsException.cs
+++ Domain/Exceptions/AlreadyExistsException.cs
@@ -1,7 +1,17 @@
+using System.Runtime.Serialization;
+
 namespace Domain.Exceptions;
 
+/// <summary>
+/// Exception which raises when we try to add an already existing item
+/// </summary>
+[Serializable]
 public class AlreadyExistsException : Exception
 {
+    protected AlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
     public AlreadyExistsException() : base("Already exists")
     {
         
